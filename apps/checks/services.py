d7b65be19341d6eb46b746beb4d502a8d3bbffbf
Mykhailo Yakubets <mikhail.yakubets@gmail.com>
2023-03-29 16:22:41 +0300

    Remove redundant function from checks services

--- apps/checks/services.py
+++ apps/checks/services.py
@@ -6,19 +6,11 @@ from .models import Printer, Check
 from .tasks import generate_check
 
 
-def get_printers_at_point(point_id: int) -> QuerySet[Printer]:
-    """
-    Get all QuerySet of Printer from db using point_id.
-    """
-    return Printer.objects.filter(point_id=point_id)
-
-
 def is_printers_at_point_exists(point_id: int) -> bool:
     """
     Check if there are printers with this point_id in db.
     """
-    printers = get_printers_at_point(point_id)
-    return printers.exists()
+    return Printer.objects.filter(point_id=point_id).exists()
 
 
 def is_checks_with_order_id_exists(order_id: int) -> bool:
@@ -28,31 +20,22 @@ def is_checks_with_order_id_exists(order_id: int) -> bool:
     return Check.objects.filter(order__order_id=order_id).exists()
 
 
-def get_list_of_check_instances(
-        printers: QuerySet[Printer], order: dict) -> list[Check]:
-    """
-    Data for bulk_create of Check instances.
-    """
-    return [
-        Check(
-            printer=printer, type=printer.check_type, order=order
-        ) for printer in printers
-    ]
-
-
 def bulk_create_checks(printers: QuerySet[Printer], order: dict) -> None:
     """
     Bulk create Checks for every printer.
     """
-    check_instances = get_list_of_check_instances(printers, order)
-    Check.objects.bulk_create(check_instances)
+    Check.objects.bulk_create([
+        Check(
+            printer=printer, type=printer.check_type, order=order
+        ) for printer in printers
+    ])
 
 
 def create_checks_at_point(point_id: int, order_id: int, order: dict) -> None:
     """
     Create Checks with PDF file for every printer at a point.
     """
-    printers = get_printers_at_point(point_id).only('check_type')
+    printers = Printer.objects.filter(point_id=point_id).only('check_type')
     bulk_create_checks(printers, order)
     group([
         generate_check.s(
